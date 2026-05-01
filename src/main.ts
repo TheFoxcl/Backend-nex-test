e786e43679228bcfab439029293c36b53cdbcd4b
TheFoxcl <juan_camilobotero@hotmail.com>
2026-05-01 08:05:43 -0500

    fixed cors configuration to allow vercel

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://fronted-nex-test.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
